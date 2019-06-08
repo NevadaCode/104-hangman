361fcef668a14132a85cdac2cd73a1cf8fb63359
Nevada Code <nevadawinkleruk@gmail.com>
2019-06-08 13:19:27 +0100

    init

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -1,22 +1,32 @@
 module Hangman
   class Game
-    attr_reader :chances, :word, :wrong_tries, :guess
+    attr_reader :chances, :word, :wrong_tries, :guess, :incorrect_guess
 
     def initialize
       @chances = 5
       @wrong_tries = 0
       @guess = ""
-      @word = Dictionary.random
+      @incorrect_guess = []
+      @word = Dictionary.random    
     end
 
     def play
       Graphics.clear_screen
       puts 'Guess this word: ' + Graphics.obfuscate_word(word, '')
+     
 
       while true
         print "[#{chances - wrong_tries} chances left]: "
 
         char = gets.chomp
+        if char == ''
+          puts "That's not a letter, silly! Please enter a letter!"
+          next
+        end
+        if !(char =~ /[[:alpha:]]/)
+          puts "That's not a letter, silly! Please enter an actual letter!"
+          next
+        end
         Graphics.clear_screen
 
         if word.include? char
@@ -36,8 +46,15 @@ module Hangman
             break
           end
         else
+          if incorrect_guess.include?(char)
+            puts "You already entered '#{char}'. No, it is still NOT correct.. 🙄"
+            puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
+            next
+          end
+
           puts "OH NOES! The word doesn't contain '#{char}'"
-          @wrong_tries = @wrong_tries + 1
+          @wrong_tries = @wrong_tries + 1    
+          incorrect_guess.push(char)
 
           if wrong_tries == chances
             puts Graphics::DEAD
